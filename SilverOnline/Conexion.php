4f134c829c3d1f2b26063c61185298effbb974c9
Luis Fernando Palomeque Ortiz <fernando18092105@gmail.com>
2019-12-02 15:15:59 -0600

    CREACION DE CONEXION A MYSQL

---
+++ SilverOnline/Conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+function conexion(){
+
+  $conexion = mysqli_connect("127.0.0.1","root,","","silver_online");
+
+  return $conexion
+}
+
+if($conexion())
+{
+  echo "Conectado...";
+}else{
+  echo "No conectado!!";
+}
+?>
