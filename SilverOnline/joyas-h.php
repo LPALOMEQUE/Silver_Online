ecfbbb309a484af3a5dd91842acfd04cd4e56ef4
Luis Fernando Palomeque Ortiz <fernando18092105@gmail.com>
2019-12-04 10:49:05 -0600

    Vista de detalle por articulo(LISTO).

--- SilverOnline/joyas-h.php
+++ SilverOnline/joyas-h.php
@@ -247,65 +247,92 @@
       </div>
     </section>
 
-    <!-- ****** Quick View Modal Area Start ****** -->
-    <div class="modal fade" id="quickview" tabindex="-1" role="dialog" aria-labelledby="quickview" aria-hidden="true">
-      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <?php
+    require_once "php/Conexion.php";
+    $con = conexion();
+    ?>
 
-          <div class="modal-body">
-            <div class="quickview_body">
-              <div class="container">
-                <div class="row">
-                  <div class="col-12 col-lg-5">
-                    <div class="quickview_pro_img">
-                      <img src="img/product-img/product-1.jpg" alt="">
+    <?php
+    $sql = "SELECT " .
+    "art.NAME_ART, " .
+    "art.PRICE, " .
+    "art.URL_IMAGE, " .
+    "ct.NAME_CAT, " .
+    "sb.NAME_SUB_CAT, " .
+    "art.ID_ARTICLES, ".
+    "art.Description ".
+    "FROM articles art " .
+    "INNER JOIN categories ct ON art.ID_CATEGORY = ct.ID_CATEGORY " .
+    "INNER JOIN sub_categories sb ON  art.ID_SUB_CATEGORY = sb.ID_SUB_CATEGORY " .
+    "where art.STATUS = 1 ";
+
+    $result = mysqli_query($con,$sql);
+    while($category = mysqli_fetch_row($result)){
+
+      ?>
+      <!-- ****** Quick View Modal Area Start ****** -->
+      <div class="modal fade" id="quickview<?php echo $category[5] ?>" tabindex="-1" role="dialog" aria-labelledby="quickview" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+
+            <div class="modal-body">
+              <div class="quickview_body">
+                <div class="container">
+
+                  <div class="row">
+
+                    <div class="col-12 col-lg-5">
+                      <div class="quickview_pro_img">
+                        <img src="<?php echo $category[2] ?>" alt="">
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-12 col-lg-7">
-                    <div class="quickview_pro_des">
-                      <h4 class="title">Boutique Silk Dress</h4>
-                      <div class="top_seller_product_rating mb-15">
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
+                    <div class="col-12 col-lg-7">
+                      <div class="quickview_pro_des">
+                        <h4 class="title"><?php echo $category[0] ?></h4>
+                        <div class="top_seller_product_rating mb-15">
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <h5 class="price">$ <?php echo $category[1] ?> <span>$624</span></h5>
+                        <p><?php echo $category[6] ?></p>
                       </div>
-                      <h5 class="price">$120.99 <span>$130</span></h5>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia expedita quibusdam aspernatur, sapiente consectetur accusantium perspiciatis praesentium eligendi, in fugiat?</p>
-                      <a href="#">View Full Product Details</a>
-                    </div>
-                    <!-- Add to Cart Form -->
-                    <form class="cart" method="post">
-                      <div class="quantity">
-                        <span class="qty-minus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 1 ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
 
-                        <input type="number" class="qty-text" id="qty" step="1" min="1" max="12" name="quantity" value="1">
+                      <!-- Add to Cart Form -->
+                      <form class="cart" method="post">
+                        <div class="quantity">
+                          <span class="qty-minus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 1 ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
 
-                        <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
-                      </div>
-                      <button type="submit" name="addtocart" value="5" class="cart-submit">Add to cart</button>
-                      <!-- Wishlist -->
-                      <div class="modal_pro_wishlist">
-                        <a href="wishlist.html" target="_blank"><i class="ti-heart"></i></a>
-                      </div>
-                      <!-- Compare -->
-                      <div class="modal_pro_compare">
-                        <a href="compare.html" target="_blank"><i class="ti-stats-up"></i></a>
-                      </div>
-                    </form>
+                          <input type="number" class="qty-text" id="qty" step="1" min="1" max="12" name="quantity" value="1">
 
-                    <div class="share_wf mt-30">
-                      <p>Share With Friend</p>
-                      <div class="_icon">
-                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                          <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                        </div>
+                        <button type="submit" name="addtocart" value="5" class="cart-submit">Add to cart</button>
+                        <!-- Wishlist -->
+                        <div class="modal_pro_wishlist">
+                          <a href="wishlist.html" target="_blank"><i class="ti-heart"></i></a>
+                        </div>
+                        <!-- Compare -->
+                        <div class="modal_pro_compare">
+                          <a href="compare.html" target="_blank"><i class="ti-stats-up"></i></a>
+                        </div>
+                      </form>
+
+                      <div class="share_wf mt-30">
+                        <p>Comparte con tus amigos</p>
+                        <div class="_icon">
+                          <a href="https://es-la.facebook.com/newsilverevolution/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                          <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                          <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                        </div>
                       </div>
+
                     </div>
                   </div>
                 </div>
@@ -314,7 +341,9 @@
           </div>
         </div>
       </div>
-    </div>
+      <?php
+    }
+    ?>
     <!-- ****** Quick View Modal Area End ****** -->
 
     <section class="shop_grid_area section_padding_100">
@@ -336,7 +365,7 @@
 
                           <!-- <li><a href="#">Hombre</a></li> -->
 
-                          <li><a href="#">Mujer</a></li>
+                          <li><a href="#joyas-m.php">Mujer</a></li>
 
                           <!-- <li><a href="#">Prom Dresses</a></li>
                           <li><a href="#">Little Black Dresses</a></li>
@@ -434,13 +463,22 @@
           $con = conexion();
 
           ?>
+
           <div class="col-12 col-md-8 col-lg-9">
             <div class="shop_grid_product_area">
               <div class="row">
-
-
                 <?php
-                $sql = "SELECT NAME_ART,PRICE,URL_IMAGE FROM articles WHERE STATUS=1";
+                $sql = "SELECT " .
+                "art.NAME_ART, " .
+                "art.PRICE, " .
+                "art.URL_IMAGE, " .
+                "ct.NAME_CAT, " .
+                "sb.NAME_SUB_CAT, " .
+                "art.ID_ARTICLES ".
+                "FROM articles art " .
+                "INNER JOIN categories ct ON art.ID_CATEGORY = ct.ID_CATEGORY " .
+                "INNER JOIN sub_categories sb ON  art.ID_SUB_CATEGORY = sb.ID_SUB_CATEGORY " .
+                "where art.STATUS = 1 ";
 
                 $result = mysqli_query($con,$sql);
                 while($category = mysqli_fetch_row($result)){
@@ -451,21 +489,21 @@
                     <div class="product-img">
                       <img src="<?php echo $category[2] ?>" alt="">
                       <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
+                        <a href="#" data-toggle="modal" data-target="#quickview<?php echo $category[5] ?>"><i class="ti-plus"></i></a>
                       </div>
                     </div>
                     <!-- Product Description -->
                     <div class="product-description">
-                      <h4 class="product-price"><?php echo $category[1] ; ?></h4>
+                      <h4 class="product-price">$<?php echo $category[1] ; ?></h4>
                       <p><?php echo $category[0] ?></p>
                       <!-- Add to Cart -->
                       <a href="#" class="add-to-cart-btn">ADD TO CART</a>
                     </div>
                   </div>
                 <?php } ?>
+
               </div>
             </div>
-
             <div class="shop_pagination_area wow fadeInUp" data-wow-delay="1.1s">
               <nav aria-label="Page navigation">
                 <ul class="pagination pagination-sm">
@@ -475,7 +513,6 @@
                 </ul>
               </nav>
             </div>
-
           </div>
         </div>
       </div>
