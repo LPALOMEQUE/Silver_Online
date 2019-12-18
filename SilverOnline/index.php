eabe42adbb93d7ee64a2603cb4d325a244e2a9ab
Luis Fernando Palomeque Ortiz <fernando18092105@gmail.com>
2019-12-18 15:22:43 -0600

    rename carga img

--- SilverOnline/index.php
+++ SilverOnline/index.php
@@ -975,7 +975,7 @@ foreach ($aCarrito as $key => $value) {
         var form_data = new FormData();
         form_data.append('file', file_data);
         $.ajax({
-          url: 'pruebaImg.php', // point to server-side PHP script
+          url: 'cargaIMG.php', // point to server-side PHP script
           dataType: 'text',  // what to expect back from the PHP script, if anything
           cache: false,
           contentType: false,
